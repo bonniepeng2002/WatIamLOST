075ea63c2f86b57976b58de3ea97bdcc14347b04
Bonnie Peng <bonniepeng2002@gmail.com>
2022-03-18 21:43:27 -0400

    map screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ClassroomFinderScreen from './src/screens/classroomFinder';
-import Header from './shared/header';
-import { Ionicons  } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ClassroomFinderScreen from "./src/screens/classroomFinder";
+import Header from "./shared/header";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Map } from "./src/screens/Map";
 const Stack = createStackNavigator();
 
 type RootStackParamList = {
@@ -18,40 +19,42 @@ const Tab = createBottomTabNavigator();
 function Classroom() {
   return (
     <Tab.Navigator
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName:any;
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: any;
 
-            if (route.name === 'Home') {
-              iconName = 'bookmark'
-            } else if (route.name === 'Settings') {
-              iconName = 'ios-list';
-            }
+          if (route.name === "Home") {
+            iconName = "bookmark";
+          } else if (route.name === "Settings") {
+            iconName = "ios-list";
+          } else if (route.name === "Map") {
+            iconName = "map"
+          }
 
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={24} color="#037A87" />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Home" component={ClassroomFinderScreen} />
-      </Tab.Navigator>
-  )
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={24} color="#037A87" />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Home" component={ClassroomFinderScreen} />
+      <Tab.Screen name="Map" component={Map} />
+    </Tab.Navigator>
+  );
 }
 
 export default function App() {
   return (
-      <NavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator
-       initialRouteName='classroomFinderScreen'
-       screenOptions={{
-        header: () => <Header />
-      }}>
-        <Stack.Screen 
-          name = "classroomFinderScreen"
-          component={Classroom}/>
+        initialRouteName="classroomFinderScreen"
+        screenOptions={{
+          header: () => <Header />,
+        }}
+      >
+        <Stack.Screen name="classroomFinderScreen" component={Classroom} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -60,8 +63,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
