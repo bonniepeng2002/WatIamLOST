b09d3e458be05016a0ae8bf8559c748a708a475d
haleysong576 <haleysong576@gmail.com>
2022-03-19 22:07:43 -0400

    WP

--- client/src/screens/classroomFinder.tsx
+++ client/src/screens/classroomFinder.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import { useState } from 'react';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
-
-
+import MapView, { Marker } from 'react-native-maps';
+import { markers } from '../data/data';
+import { FontAwesome  } from '@expo/vector-icons';
 
 const ClassroomFinderScreen = () => {
     const [region, setRegion] = useState({
@@ -22,15 +22,26 @@ const ClassroomFinderScreen = () => {
     const onRegionChange = (region: region) =>  {
         setRegion(region);
     }
-    
+    const buildings = markers;
+    const _map = React.useRef(null);
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.tsx to start working on your app!</Text> */}
       <MapView
+        ref={_map}
         style={styles.map}
         region={region}
         onRegionChange={onRegionChange}
-      />
+      >
+        {buildings.map((building, index)=>{
+          return (
+            <Marker key={index} coordinate={building.coordinate}>
+              <FontAwesome name="map-marker" size={26} color="red" />
+            </Marker>
+          )
+        })}
+
+      </MapView>
 
     </View>
   );
@@ -45,7 +56,7 @@ const styles = StyleSheet.create({
   },
   map: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height * 0.5,
+    height: Dimensions.get('window').height,
   },
 });
 
